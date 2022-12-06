4d5e6b7cabf9481b0b322b08a1c6dc20da45ff4f
mincheol-Jeon <alsehf0714@naver.com>
2022-12-06 22:40:58 +0900

    raw 데이터 뿌려주는 초기 함수

--- package/__init__.py
+++ package/__init__.py
@@ -22,11 +22,39 @@ def get_db_connection(config):
     )
     return connection
 
+def fetch(query,cursor,data_type = 'dataframe'):
+    cursor.execute(query)
+
+    if data_type == 'dataframe':
+        
+        col = cursor.description
+        col_list = [col[i][0] for i in range(len(col))]
+        res = pd.DataFrame(cursor.fetchall(),columns = col_list)
+        
+        return res
+    
+    if data_type == 'raw':
+        res = cursor.fetchall()
+        return str(res)
+        
+    
+
 app = Flask(__name__)
+conn = get_db_connection(get_db_config())
+cursor = conn.cursor()
 
 @app.route('/')
-
 def index():
     return 'Hello Flask!'
+
+@app.route('/test')
+def test_page():
+    param = input()
+    res = fetch(query = ''' select corp_name,corp_code,stock_code,url,rcept_dt
+                            from tmp 
+                            where corp_name = "{}" order by rcept_dt limit 5;'''.format(param),
+                cursor = cursor,
+                data_type = 'raw')
+    return res
     
 app.run(host='0.0.0.0',port=50)
